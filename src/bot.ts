c748debff6a73bcb1217ae3000db4aa7cbb94143
3xp3ct3d <108523972+sufreqqq@users.noreply.github.com>
2022-10-29 00:09:07 +0300

    minor update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -2,10 +2,10 @@ import * as config from './config.json'
 import * as configGas from './configGas.json'
 import { CurrencyMonitor, GasTracker } from './currencyMonitor'
 
-//config.bots.forEach(bot => {
-//    const currencyMonitor = new CurrencyMonitor(bot.symbol, bot.token)
-//    currencyMonitor.run()
-//})
+config.bots.forEach(bot => {
+    const currencyMonitor = new CurrencyMonitor(bot.symbol, bot.token)
+    currencyMonitor.run()
+})
 
 configGas.bot.forEach(bot =>{
     const gasTracker = new GasTracker(bot.token)
